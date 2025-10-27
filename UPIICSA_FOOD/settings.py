1178f2d33489602ab1d084feee15bd4b886aee64
Zaid Alberto <zramirezh1801@alumno.ipn.mx>
2025-10-26 21:03:55 -0600

    Fix: Configuración de Vercel corregida

--- UPIICSA_FOOD/settings.py
+++ UPIICSA_FOOD/settings.py
@@ -120,10 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'UPIICSA_STORE/static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Vercel doesn't support collectstatic, so we serve from the app static folder
+if os.environ.get('VERCEL'):
+    STATICFILES_DIRS = []
+else:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'UPIICSA_STORE/static'),
+    ]
 
 # WhiteNoise configuration for static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- build_files.sh
+++ build_files.sh
@@ -4,9 +4,9 @@
 echo "Building project..."
 
 # Install dependencies
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput --clear
 
 echo "Build completed successfully!"
