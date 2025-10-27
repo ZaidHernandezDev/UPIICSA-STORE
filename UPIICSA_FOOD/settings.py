37970d8f01af0dfac7c13c462a3529559da39b32
Zaid Alberto <zramirezh1801@alumno.ipn.mx>
2025-10-26 21:55:55 -0600

    Prueba subir estaticos manual

--- UPIICSA_FOOD/settings.py
+++ UPIICSA_FOOD/settings.py
@@ -120,20 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Different static configuration for Vercel vs Local
-if os.environ.get('VERCEL'):
-    # In Vercel, use the app static folder directly (no collectstatic)
-    STATIC_ROOT = None
-    STATICFILES_DIRS = []
-    WHITENOISE_USE_FINDERS = True
-    WHITENOISE_AUTOREFRESH = True
-else:
-    # In local, use traditional collectstatic approach
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'UPIICSA_STORE/static'),
-    ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'UPIICSA_STORE/static'),
+]
 
 # WhiteNoise configuration for static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
